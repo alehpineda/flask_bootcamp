5824fdbef0d155d1635c182149e03b7315d4b018
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-09 10:32:06 -0500

    Update 00-Flask-forms-basics.py
    Added comments

--- 04-Flask-forms/00-Flask-forms-basics.py
+++ 04-Flask-forms/00-Flask-forms-basics.py
@@ -1,27 +1,34 @@
 from flask import Flask, render_template
+# Imports for the forms
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
 
+# config secret key
 app.config['SECRET_KEY'] = 'my_super_secret_key'
 
+# declare form class
 class InfoForm(FlaskForm):
 
     breed = StringField('What Breed are you?')
     submit = SubmitField('Submit')
 
 
+# route with methods get and post
 @app.route('/', methods=['GET', 'POST'])
 def index():
     breed = False
-
+    #instanciate the form
     form = InfoForm()
-
+    # validate the form
     if form.validate_on_submit():
+        # get data from form
         breed = form.breed.data
+        # clear the field form
         form.breed.data = ''
     
+    # pass the form and variable to the template
     return render_template('index.html', form=form, breed=breed)
 
 
